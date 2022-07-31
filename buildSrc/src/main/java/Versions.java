bd46359f9a93210312cef99b6c5ce9a1f776e9a0
Сулаков Антон <sulakov_as@magnit.ru>
2022-07-31 22:27:33 +0300

    Homework 17 multiprocess initial commit

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -5,5 +5,9 @@ public interface Versions {
     String jetty = "11.0.7";
     String freemarker = "2.3.31";
     String jsr305 = "3.0.2";
+    String protobuf = "3.19.4";
+    String grpc = "1.44.1";
+    String errorProneAnnotations = "2.11.0";
+    String tomcatAnnotationsApi = "6.0.53";
 
 }
